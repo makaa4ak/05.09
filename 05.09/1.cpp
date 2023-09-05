8632a073beab65aa11be066b784ed9d5431f56d9
Ganic_wi63@student.itstep.org <Ganic_wi63@student.itstep.org>
2023-09-05 10:27:46 +0300

    Create func2, func3

---
+++ 05.09/1.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+void func1()
+{
+	cout << "Func 1\n" << endl;
+}
+
+void func2()
+{
+	cout << "Func 2\n" << endl;
+}
+
+void func3()
+{
+	cout << "Func 3\n" << endl;
+}
+
+int main() 
+{
+
+
+}
